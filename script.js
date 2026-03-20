c7dd3924665953a419755773594588897b16f1c5
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-03-20 14:43:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const count=document.getElementById("counter");
+const btn =document.getElementById("incrementBtn");
+btn.addEventListener('click',()=>{
+	let currVal = Number(count.textContent)
+	alert(currVal)
+	count.textContent=currVal+1
+});
